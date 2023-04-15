7c71c10f1e8468d930237630390aa0c74d0abf49
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-04-15 13:48:16 +0300

    Add files via upload

---
+++ ConsoleApp_SRP/ConsoleApp_SRP/DB_TemelIslemler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SRP
+{
+    public class DB_TemelIslemler
+    {
+        public void Kaydet()
+        {
+            Console.WriteLine("kaydediliyor...");
+        }
+        public void Sil()
+        {
+            Console.WriteLine("Siliniyor...");
+        }
+        public void Baglan()
+        {
+            Console.WriteLine("Baglaniyor...");
+        }
+    }
+
+    public class Raporlama
+    {
+        public void RaporOlustur()
+        {
+            Console.WriteLine("RaporOlusturuluyor...");
+        }
+        public void RaporYukle()
+        {
+            Console.WriteLine("RaporYukleniyor...");
+        }
+
+        public void RaporSil()
+        {
+            Console.WriteLine("RaporSiliniyor...");
+        }
+    }
+}
---
+++ ConsoleApp_SRP/ConsoleApp_SRP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DB_TemelIslemler dB_TemelIslemler = new DB_TemelIslemler();
+            dB_TemelIslemler.Baglan();
+
+            Raporlama raporlama = new Raporlama();
+            raporlama.RaporOlustur();
+        }
+    }
+}
