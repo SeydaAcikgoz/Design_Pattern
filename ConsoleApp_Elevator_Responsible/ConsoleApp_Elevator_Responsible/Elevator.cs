0cbe68ce4bbe37e48f9a54ccf17ac38ed3db3b41
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-04-17 18:37:02 +0300

    Add files via upload

---
+++ ConsoleApp_Elevator_Responsible/ConsoleApp_Elevator_Responsible/Elevator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Elevator_Responsible
+{
+    public interface Elevator
+    {
+
+        public void go(Floor desiredFloor);
+
+        public void openDoor();
+
+        public void closeDoor();
+
+    }
+}
---
+++ ConsoleApp_Elevator_Responsible/ConsoleApp_Elevator_Responsible/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp_Elevator_Responsible
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            Elevator elevator = new ResponsibleElevator();
+            elevator.openDoor();  // Get into the elevator
+            elevator.go(Floor.FIFTH);
+
+            elevator.openDoor(); // Somebody is coming :)
+            elevator.go(Floor.FIRST);
+            elevator.go(Floor.FIFTH);
+        }
+    }
+}
