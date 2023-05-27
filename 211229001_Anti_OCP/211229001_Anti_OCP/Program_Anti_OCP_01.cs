076f129170077a38456621d00e2b8a8ed9bea360
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-05-27 21:08:23 +0300

    Update Program_Anti_OCP_01.cs

--- 211229001_Anti_OCP/211229001_Anti_OCP/Program_Anti_OCP_01.cs
+++ 211229001_Anti_OCP/211229001_Anti_OCP/Program_Anti_OCP_01.cs
@@ -61,6 +61,8 @@ namespace _211229001_Anti_OCP
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
+            
+            
             customer.LogIn(5, "seyda@gmail.com", "123Seyda");
             Console.WriteLine();
             customer.LogIn(1, "seda@gmail.com", "123Seyda");
