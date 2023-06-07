0fb5f0d3e5b811135cabb8e29a5a65ecf7dff078
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-06-07 23:12:27 +0300

    Update NotEnoughBalanceException.cs

--- ConsoleApp_Command/ConsoleApp_Command/NotEnoughBalanceException.cs
+++ ConsoleApp_Command/ConsoleApp_Command/NotEnoughBalanceException.cs
@@ -4,7 +4,6 @@ namespace ConsoleApp_Command
 {
     public class NotEnoughBalanceException : Exception
     {
-
         private static String description = "Hesapta yeterli bakiye yoktur: Miktar: ";
 
         public NotEnoughBalanceException(int balance, int amount) : base(description + balance + " Amount: " + amount)
