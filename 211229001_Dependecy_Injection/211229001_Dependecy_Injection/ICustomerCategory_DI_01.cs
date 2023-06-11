b424d8f8f2b72cb9d59eea38af36a212f75db7ba
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-06-11 20:07:08 +0300

    Update ICustomerCategory_DI_01.cs

--- 211229001_Dependecy_Injection/211229001_Dependecy_Injection/ICustomerCategory_DI_01.cs
+++ 211229001_Dependecy_Injection/211229001_Dependecy_Injection/ICustomerCategory_DI_01.cs
@@ -8,7 +8,6 @@ namespace _211229001_Dependecy_Injection
 {
     public interface ICustomerCategory_DI_01
     {
-
         public Customer_DI_01 Create(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
     }
 }
