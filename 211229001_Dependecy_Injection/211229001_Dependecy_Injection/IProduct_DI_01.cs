949d6f420753a0ee8d5cdb12dff4ef6c6b05b093
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-06-11 20:06:45 +0300

    Update IProduct_DI_01.cs

--- 211229001_Dependecy_Injection/211229001_Dependecy_Injection/IProduct_DI_01.cs
+++ 211229001_Dependecy_Injection/211229001_Dependecy_Injection/IProduct_DI_01.cs
@@ -9,7 +9,6 @@ namespace _211229001_Dependecy_Injection
     public interface IProduct_DI_01
     {
         public Beverages_DI_01 CreateBeverages(int code, string ProductName, string category, int Stock, double Price);
-
         public Confections_DI_01 CreateConfectionns(int code, string ProductName, string category, int Stock, double Price);
         public GrainsCereals_DI_01 CreateGrainsCereals(int code, string ProductName, string category, int Stock, double Price);
     }
