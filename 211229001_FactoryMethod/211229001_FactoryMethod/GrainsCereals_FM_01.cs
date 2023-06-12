d10d1e4fb714b1ce2d774271ba72f01a9b2cb942
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-06-12 23:26:07 +0300

    Update GrainsCereals_FM_01.cs

--- 211229001_FactoryMethod/211229001_FactoryMethod/GrainsCereals_FM_01.cs
+++ 211229001_FactoryMethod/211229001_FactoryMethod/GrainsCereals_FM_01.cs
@@ -19,7 +19,6 @@ namespace _211229001_FactoryMethod
 
         public GrainsCereals_FM_01()
         {
-
         }
 
         public GrainsCereals_FM_01 Create(int code, string ProductName, int category, int Stock, double Price)
