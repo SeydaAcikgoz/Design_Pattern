a9175de3d21571066157c597264788e8688dab43
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-09 21:46:09 +0300

    Update Program_Anti_OCP_01.cs

--- 211229001_Anti_OCP/211229001_Anti_OCP/Program_Anti_OCP_01.cs
+++ 211229001_Anti_OCP/211229001_Anti_OCP/Program_Anti_OCP_01.cs
@@ -9,7 +9,6 @@ namespace _211229001_Anti_OCP
     {
         static void Main(string[] args)
         {
-
             Products_Anti_OCP_01 product = new Products_Anti_OCP_01();
             product.ProductsList.Add(new Products_Anti_OCP_01() { Code = 101, ProductName = "Chai", categoryType = CategoryType.Beverages, Stock = 39, Price = 18 });
             product.ProductsList.Add(new Products_Anti_OCP_01() { Code = 102, ProductName = "Chang", categoryType = CategoryType.Beverages, Stock = 17, Price = 19 });
@@ -21,10 +20,8 @@ namespace _211229001_Anti_OCP
             product.ProductsList.Add(new Products_Anti_OCP_01() { Code = 302, ProductName = "Tofu", categoryType = CategoryType.GrainsCereals, Stock = 35, Price = 25.25 });
             product.ProductsList.Add(new Products_Anti_OCP_01() { Code = 303, ProductName = "Tunnbröd", categoryType = CategoryType.GrainsCereals, Stock = 61, Price = 9 });
 
-
             Customer_Anti_OCP_01 customer = new Customer_Anti_OCP_01();
             customer.CustomerList.Add(new Customer_Anti_OCP_01() { UserNo = 1, UserName = "Seyda", UserSurname = "Acikgoz", Adress = "Konya", UserAmount = 1230, Point = 15, customerType = CustomerType.Bronz, Email = "seyda@gmail.com", Password = "123Seyda", LoginStatus = false });
-
 
             product.LogIn(5, "seyda@gmail.com", "123Seyda", customer.CustomerList);
             Console.WriteLine();
@@ -57,11 +54,9 @@ namespace _211229001_Anti_OCP
             Console.WriteLine();
             product.LogOut(1, customer.CustomerList);
 
-
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine();
-            
             
             customer.LogIn(5, "seyda@gmail.com", "123Seyda");
             Console.WriteLine();
@@ -93,7 +88,6 @@ namespace _211229001_Anti_OCP
             customer.ShowProductsCategory(CategoryType.Beverages, product.ProductsList);
             Console.WriteLine();
             customer.LogOut(1);
-
         }
     }
 }
