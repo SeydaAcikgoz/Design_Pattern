6b34d19e582515d4497adea3b48b5df0ba830a23
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-10 14:11:23 +0300

    Update IProductTransactions_LSP_01.cs

--- 211229001_LSP/IProductTransactions_LSP_01.cs
+++ 211229001_LSP/IProductTransactions_LSP_01.cs
@@ -9,16 +9,10 @@ namespace _211229001_LSP
     public interface IProductTransactions_LSP_01
     {
         public void LogIn(int cod, string email, string password, List<Customer_LSP_01> CustomerList);
-
         public void AddBasket(int USerCode, int ProductCode, List<Products_LSP_01> ProductsList);
-
         public void ShowBasket();
-
         public void FindPrice(int ProductCode, List<Products_LSP_01> ProductsList);
-
         public void FindStock(int ProductCode, List<Products_LSP_01> ProductsList);
-
         public void ShowProductsCategory(int category, List<Products_LSP_01> ProductsList);
-
     }
 }
