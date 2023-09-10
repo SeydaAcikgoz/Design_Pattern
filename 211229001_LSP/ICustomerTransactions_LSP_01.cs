7569ce3b070e29b50b7e1c5c4f1723d12dcfaa84
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-10 14:11:48 +0300

    Update ICustomerTransactions_LSP_01.cs

--- 211229001_LSP/ICustomerTransactions_LSP_01.cs
+++ 211229001_LSP/ICustomerTransactions_LSP_01.cs
@@ -9,11 +9,8 @@ namespace _211229001_LSP
     public interface ICustomerTransactions_LSP_01
     {
         public void LogOut(int UserNo, List<Customer_LSP_01> CustomerList);
-
         public void Buy(int UserCode, List<Products_LSP_01> ProductsList, List<Customer_LSP_01> CustomerList);
-
         public void Shipping(Customer_LSP_01 customer);
         public void ShowAccount(int Usercode, List<Customer_LSP_01> CustomerList);
-
     }
 }
