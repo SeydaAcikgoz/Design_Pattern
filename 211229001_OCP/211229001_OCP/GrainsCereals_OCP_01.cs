b784d9f89eb41a0bfa3fcea0c1965c0c09210f90
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-13 18:55:15 +0300

    Update GrainsCereals_OCP_01.cs

--- 211229001_OCP/211229001_OCP/GrainsCereals_OCP_01.cs
+++ 211229001_OCP/211229001_OCP/GrainsCereals_OCP_01.cs
@@ -8,7 +8,6 @@ namespace _211229001_OCP
 {
     public class GrainsCereals_OCP_01 : Products_OCP_01
     {
-
         public GrainsCereals_OCP_01(int code, string ProductName, int CategoryNo, string category, int Stock, double Price)
         {
             this.Code = code;
@@ -21,7 +20,6 @@ namespace _211229001_OCP
 
         public GrainsCereals_OCP_01()
         {
-
         }
     }
 }
