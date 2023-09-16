8c259b08144d8de37abf112f5be513c5cce5f883
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-16 13:02:14 +0300

    Update ICustomerTranscations_ISP_01.cs

--- 211229001_ISP/ICustomerTranscations_ISP_01.cs
+++ 211229001_ISP/ICustomerTranscations_ISP_01.cs
@@ -9,12 +9,8 @@ namespace _211229001_ISP
     public interface ICustomerTranscations_ISP_01
     {
         public void LogOut(int UserNo, List<Customer_ISP_01> CustomerList);
-
         public void Buy(int UserCode, List<Products_ISP_01> ProductsList, List<Customer_ISP_01> CustomerList);
-
         public void Shipping(Customer_ISP_01 customer);
         public void ShowAccount(int Usercode, List<Customer_ISP_01> CustomerList);
-
-
     }
 }
