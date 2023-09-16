b0ea3986ffe8ec13ca638b61ab24e0dd369bafc2
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-16 13:02:04 +0300

    Update IProductsTranscations_ISP_01.cs

--- 211229001_ISP/IProductsTranscations_ISP_01.cs
+++ 211229001_ISP/IProductsTranscations_ISP_01.cs
@@ -9,16 +9,10 @@ namespace _211229001_ISP
     public interface IProductsTranscations_ISP_01
     {
         public void LogIn(int cod, string email, string password, List<Customer_ISP_01> CustomerList);
-
         public void AddBasket(int USerCode, int ProductCode, List<Products_ISP_01> ProductsList);
-
         public void ShowBasket();
-
         public void FindPrice(int ProductCode, List<Products_ISP_01> ProductsList);
-
         public void FindStock(int ProductCode, List<Products_ISP_01> ProductsList);
-
         public void ShowProductsCategory(CategoryType category, List<Products_ISP_01> ProductsList);
-
     }
 }
