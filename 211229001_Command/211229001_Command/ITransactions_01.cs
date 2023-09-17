6b715172be4cea6f87eb8614b2d962cee77fa4b9
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-17 22:53:57 +0300

    Update ITransactions_01.cs

--- 211229001_Command/211229001_Command/ITransactions_01.cs
+++ 211229001_Command/211229001_Command/ITransactions_01.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace _211229001_Command
 {
-    internal interface ITransactions_01
+    public interface ITransactions_01
     {
         public void Execute(int product, List<Products_01> basket);
-
         public void Undo(List<Products_01> basket);
-
         public void Redo(int product, List<Products_01> basket);
     }
 }
