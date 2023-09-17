71458a4b7ed74e2f036055c835ecd6f37d672c13
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-17 22:54:13 +0300

    Update ITransactions2_01.cs

--- 211229001_Command/211229001_Command/ITransactions2_01.cs
+++ 211229001_Command/211229001_Command/ITransactions2_01.cs
@@ -9,7 +9,6 @@ namespace _211229001_Command
     public interface ITransactions2_01
     {
         public void Execute(int code, string ProductName, string category, int Stock, double Price, List<Products_01> ProductsList);
-
         public void Redo(int code, string ProductName, string category, int Stock, double Price, List<Products_01> ProductsList);
     }
 }
