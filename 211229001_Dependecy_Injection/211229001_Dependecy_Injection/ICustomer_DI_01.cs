afb021724290a41ade75e04b1bd5cac9c9062ba6
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-18 16:53:05 +0300

    Update ICustomer_DI_01.cs

--- 211229001_Dependecy_Injection/211229001_Dependecy_Injection/ICustomer_DI_01.cs
+++ 211229001_Dependecy_Injection/211229001_Dependecy_Injection/ICustomer_DI_01.cs
@@ -9,11 +9,8 @@ namespace _211229001_Dependecy_Injection
     public interface ICustomer_DI_01
     {
         public BronzCustomer_DI_01 CreateBronzCustomer(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
-
         public GoldCustomer_01 CreateGoldzCustomer(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
-
         public SilverCustomer_DI_01 CreateSilverCustomer(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
-
         public StandardCustomer_DI_01 CreateStandardCustomer(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
     }
 }
