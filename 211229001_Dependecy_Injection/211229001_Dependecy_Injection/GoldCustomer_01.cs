21479b91071bef6dd4516cfa4f3e70a1129cf15f
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-18 16:50:15 +0300

    Update GoldCustomer_01.cs

--- 211229001_Dependecy_Injection/211229001_Dependecy_Injection/GoldCustomer_01.cs
+++ 211229001_Dependecy_Injection/211229001_Dependecy_Injection/GoldCustomer_01.cs
@@ -25,6 +25,7 @@ namespace _211229001_Dependecy_Injection
         public GoldCustomer_01()
         {
         }
+        
         public Customer_DI_01 Create(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus)
         {
             Customer_DI_01 customer = new Customer_DI_01(userno, userName, userSurname, adress, UserAmount, point, customerType, email, password, loginStatus);
