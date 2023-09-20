fbe18adc2738b640e650a7cffbdd360102afe748
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-20 14:05:30 +0300

    Update Beverages_FM_01.cs

--- 211229001_FactoryMethod/211229001_FactoryMethod/Beverages_FM_01.cs
+++ 211229001_FactoryMethod/211229001_FactoryMethod/Beverages_FM_01.cs
@@ -19,7 +19,6 @@ namespace _211229001_FactoryMethod
 
         public Beverages_FM_01()
         {
-
         }
 
         public Beverages_FM_01 Create(int code, string ProductName, int category, int Stock, double Price)
