28c9cfd5e74b6e25255007c6c54c4ea7e0a621f3
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-20 14:07:21 +0300

    Update ISilverCustomerFactory_FM_01.cs

--- 211229001_FactoryMethod/211229001_FactoryMethod/ISilverCustomerFactory_FM_01.cs
+++ 211229001_FactoryMethod/211229001_FactoryMethod/ISilverCustomerFactory_FM_01.cs
@@ -9,6 +9,5 @@ namespace _211229001_FactoryMethod
     public interface ISilverCustomerFactory_FM_01
     {
         public SilverCustomer_FM_01 Create(int userno, string userName, string userSurname, string adress, double UserAmount, int point, string customerType, string email, string password, bool loginStatus);
-
     }
 }
