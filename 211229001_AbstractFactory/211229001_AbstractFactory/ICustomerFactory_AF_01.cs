4426c00ae5b2a767d2cb4723276acd20b7c15ecc
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-23 18:32:39 +0300

    Update ICustomerFactory_AF_01.cs

--- 211229001_AbstractFactory/211229001_AbstractFactory/ICustomerFactory_AF_01.cs
+++ 211229001_AbstractFactory/211229001_AbstractFactory/ICustomerFactory_AF_01.cs
@@ -9,11 +9,8 @@ namespace _211229001_AbstractFactory
     public interface ICustomerFactory_AF_01
     {
         public BronzCustomer_AF_01 CreateBronzCustomer(List<Customer_AF_01> CustomerList);
-
         public GoldCustomer_AF_01 CreateGoldCustomer(List<Customer_AF_01> CustomerList);
-
         public SilverCustomer_AF_01 CreateSilverCustomer(List<Customer_AF_01> CustomerList);
-
         public StandardCustomer_AF_01 CreateStandardCustomer(List<Customer_AF_01> CustomerList);
     }
 }
