4661ef60f1e162b0c2f7914404a2010f89ba6425
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-23 18:32:57 +0300

    Update IProductFactory_AF_01.cs

--- 211229001_AbstractFactory/211229001_AbstractFactory/IProductFactory_AF_01.cs
+++ 211229001_AbstractFactory/211229001_AbstractFactory/IProductFactory_AF_01.cs
@@ -9,9 +9,7 @@ namespace _211229001_AbstractFactory
     public interface IProductFactory_AF_01
     {
         public Beverages_AF_01 CreateBeverages(List<Products_AF_01> ProductsList);
-
         public Confections_AF_01 CreateConfections(List<Products_AF_01> ProductsList);
-
         public GrainsCereals_AF_01 CreateGrainsCereals(List<Products_AF_01> ProductsList);
     }
 }
