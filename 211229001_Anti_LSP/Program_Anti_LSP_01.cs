0da0c009784025913b5fb2348285a4a36c1ff2c4
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-26 21:08:35 +0300

    Update Program_Anti_LSP_01.cs

--- 211229001_Anti_LSP/Program_Anti_LSP_01.cs
+++ 211229001_Anti_LSP/Program_Anti_LSP_01.cs
@@ -8,7 +8,6 @@ namespace _211229001_Anti_LSP
     {
         static void Main(string[] args)
         {
-
             Products_Anti_LSP_01 product = new Products_Anti_LSP_01();
             product.ProductsList.Add(new Products_Anti_LSP_01() { Code = 101, ProductName = "Chai", categoryType = CategoryType.Beverages, Stock = 39, Price = 18 });
             product.ProductsList.Add(new Products_Anti_LSP_01() { Code = 102, ProductName = "Chang", categoryType = CategoryType.Beverages, Stock = 17, Price = 19 });
@@ -20,10 +19,8 @@ namespace _211229001_Anti_LSP
             product.ProductsList.Add(new Products_Anti_LSP_01() { Code = 302, ProductName = "Tofu", categoryType = CategoryType.GrainsCereals, Stock = 35, Price = 25.25 });
             product.ProductsList.Add(new Products_Anti_LSP_01() { Code = 303, ProductName = "Tunnbröd", categoryType = CategoryType.GrainsCereals, Stock = 61, Price = 9 });
 
-
             Customer_Anti_LSP_01 customer = new Customer_Anti_LSP_01();
             customer.CustomerList.Add(new Customer_Anti_LSP_01() { UserNo = 1, UserName = "Seyda", UserSurname = "Acikgoz", Adress = "Konya", UserAmount = 1230, Point = 15, customerType = CustomerType.Bronz, Email = "seyda@gmail.com", Password = "123Seyda", LoginStatus = false });
-
 
             product.LogIn(5, "seyda@gmail.com", "123Seyda", customer.CustomerList);
             Console.WriteLine();
@@ -55,12 +52,10 @@ namespace _211229001_Anti_LSP
             product.ShowProductsCategory(CategoryType.Beverages);
             Console.WriteLine();
             product.LogOut(1, customer.CustomerList);
-
             
             Console.Writline();
             Console.Writline();
             Console.Writline();
-            
             
             customer.LogIn(5, "seyda@gmail.com", "123Seyda");
             Console.WriteLine();
@@ -92,7 +87,6 @@ namespace _211229001_Anti_LSP
             customer.ShowProductsCategory(CategoryType.Beverages, product.ProductsList);
             Console.WriteLine();
             customer.LogOut(1);
-
         }
     }
 }
