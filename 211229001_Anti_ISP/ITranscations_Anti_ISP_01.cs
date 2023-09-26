0609de2d6223844b81d0e59328d7772c0ca629b8
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2023-09-26 21:04:06 +0300

    Update ITranscations_Anti_ISP_01.cs

--- 211229001_Anti_ISP/ITranscations_Anti_ISP_01.cs
+++ 211229001_Anti_ISP/ITranscations_Anti_ISP_01.cs
@@ -6,24 +6,17 @@ using System.Threading.Tasks;
 
 namespace _211229001_Anti_ISP
 {
-    internal interface ITranscations_Anti_ISP_01
+    public interface ITranscations_Anti_ISP_01
     {
         public void LogIn(int cod,string email, string password, List<Customer_Anti_ISP_01> CustomerList);
         public void LogOut(int UserNo, List<Customer_Anti_ISP_01> CustomerList);
-
         public void Buy(int UserCode, List<Products_Anti_ISP_01> ProductsList, List<Customer_Anti_ISP_01> CustomerList);
-
         public void AddBasket(int USerCode, int ProductCode, List<Products_Anti_ISP_01> ProductsList);
-
         public void ShowBasket();
         public void Shipping(Customer_Anti_ISP_01 customer);
-
         public void FindPrice(int ProductCode, List<Products_Anti_ISP_01> ProductsList);
-
         public void FindStock(int ProductCode, List<Products_Anti_ISP_01> ProductsList);
-
         public void ShowAccount(int Usercode, List<Customer_Anti_ISP_01> CustomerList);
-
         public void ShowProductsCategory(CategoryType category, List<Products_Anti_ISP_01> ProductsList);
     }
 }
